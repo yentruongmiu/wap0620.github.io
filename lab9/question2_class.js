ab84bb4064d4cf8c4443bf9344e38c9e51349b3f
Yen Truong <truthblue82@gmail.com>
2022-07-03 20:54:53 -0500

    lab 9 solution class

--- lab9/question2_class.js
+++ lab9/question2_class.js
@@ -1,6 +1,54 @@
+class Question {
+  constructor(questionId, answer) {
+    this.questionId = questionId;
+    this.answer = answer;
+  }
+
+  checkAnswer(correctAnswer) {
+    return this.answer === correctAnswer;
+  }
+}
+
+class Student {
+  constructor(studentId, answers = []) {
+    this.studentId = studentId;
+    this.answers = answers;
+  }
+
+  addAnswer(question) {
+    this.answers.push(question);
+  }
+}
+
+class Quiz {
+  constructor(questionArr = [], students = []) {
+    this.questions = new Map();
+    questionArr.forEach(question => this.questions.set(question.questionId, question.answer));
+    
+    this.students = students;
+  }
+
+  scoreStudentBySid(studentId) {
+    let student = this.students.filter(stu => stu.studentId === studentId)[0];
+
+    return student.answers.reduce((sum, currentQuestion) => {
+      let questionId = currentQuestion.questionId;
+      let correctAnswer = this.questions.get(questionId);
+      let result = currentQuestion.checkAnswer(correctAnswer);
+      if (result) {
+        sum += 1;
+      }
+      return sum;
+    }, 0);
+  }
+
+  getAverageScore() {
+    return this.students.reduce((average, currentStudent, idx, array) => average + this.scoreStudentBySid(currentStudent.studentId) / array.length, 0);
+  }
+}
 
 
-
+const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
 
 const student1 = new Student(10);
 student1.addAnswer(new Question(2, 'a'));
@@ -13,7 +61,6 @@ student2.addAnswer(new Question(2, 'a'));
 student2.addAnswer(new Question(1, 'd'));
 
 const students = [student1, student2];
-const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
 
 const quiz = new Quiz(questions, students);
 let scoreForStudent10 = quiz.scoreStudentBySid(10);
--- lab9/question2_contructor_function.js
+++ lab9/question2_contructor_function.js
@@ -4,10 +4,7 @@ function Question(qid, answer) {
 }
 
 Question.prototype.checkAnswer = function (answer) {
-  console.log(this.answer, answer);
-  let flg = this.answer === answer;
-  console.log(flg);
-  return flg;
+  return this.answer === answer;
 }
 
 function Student(studentId) {
@@ -21,14 +18,14 @@ Student.prototype.addAnswer = function(question) {
 
 function Quiz(questions, students) {
   this.questions = new Map();
-  questions.forEach(q => this.questions.set(q.id, q.answer));
+  questions.forEach(q => this.questions.set(q.qid, q.answer));
 
   this.students = students;
 }
 
 Quiz.prototype.scoreStudentBySid = function (studentId) {
   const student = this.students.filter(stu => stu.studentId === studentId)[0];
-
+  
   return student.answers.reduce((sum, currentQuestion) => {
     if (currentQuestion.checkAnswer(this.questions.get(currentQuestion.qid))) {
       sum = sum + 1;
@@ -43,22 +40,13 @@ Quiz.prototype.getAverageScore = function () {
   }, 0);
 }
 
-const q1 = new Question(1, 'b');
-const q2 = new Question(2, 'a');
-const q3 = new Question(3, 'b');
 
-//const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
-
-const questions = [q1, q2, q3];
+const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
 
 const student1 = new Student(10);
-/*student1.addAnswer(new Question(2, 'a'));
+student1.addAnswer(new Question(2, 'a'));
 student1.addAnswer(new Question(3, 'b'));
 student1.addAnswer(new Question(1, 'b'));
-*/
-student1.addAnswer(q2);
-student1.addAnswer(q3);
-student1.addAnswer(q1);
 
 const student2 = new Student(11);
 student2.addAnswer(new Question(3, 'b'));
@@ -66,7 +54,7 @@ student2.addAnswer(new Question(2, 'a'));
 student2.addAnswer(new Question(1, 'd'));
 
 const students = [student1, student2];
-console.log(JSON.stringify(students));
+
 const quiz = new Quiz(questions, students);
 let scoreForStudent10 = quiz.scoreStudentBySid(10);
 console.log(scoreForStudent10); // Result: 3
