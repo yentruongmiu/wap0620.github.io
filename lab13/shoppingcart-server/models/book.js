69d231629e4b6353c2ecba896c52623c1d82a344
Yen Truong <truthblue82@gmail.com>
2022-07-07 22:01:58 -0500

    add lab13

---
+++ lab13/shoppingcart-server/models/book.js
@@ -0,0 +1,53 @@
+let books = [];
+let counter = 0;
+
+module.exports = class Book {
+  constructor(id, title, ISBN, publishedDate, author) {
+    //id, title, ISBN, publishedDate, author
+    this.id = id;
+    this.title = title;
+    this.ISBN = ISBN;
+    this.publishedDate = publishedDate;
+    this.author = author;
+  }
+
+  save() {
+    this.id = ++counter;
+    books.push(this);
+    return this;
+  }
+
+  update() {
+    const index = books.findIndex(b => b.id == this.id);
+    if (index > -1) {
+        books.splice(index, 1, this);
+        return this;
+    } else {
+        throw new Error('NOT Found');
+    }
+  }
+
+  static fetchAll() {
+    return books;
+  }
+
+  static findById(bookId) {
+    const index = books.findIndex(b => b.id == bookId);
+    
+    if (index > -1) {
+      return books[index];
+    } else {
+      throw new Error('NOT Found');
+    }
+  }
+
+  static deleteById(bookId) {
+    const index = books.findIndex(b => b.id == bookId);
+    if (index > -1) {
+        books = books.filter(b => b.id != bookId);
+    } else {
+        throw new Error('NOT Found');
+    }
+  }
+
+}
---
+++ lab13/shoppingcart-server/readme.md
@@ -0,0 +1,23 @@
+    {
+        "id": 1,
+        "title": "Nodejs",
+        "ISBN": "1000",
+        "publishedDate": "2020-02-12",
+        "author": "John Smith"
+    },
+    {
+        "id": 2,
+        "title": "Angular 2",
+        "ISBN": "1001",
+        "publishedDate": "2020-03-12",
+        "author": "Peter Pan"
+    },
+    {
+        "id": 3,
+        "title": "Java for beginner",
+        "ISBN": "1001",
+        "publishedDate": "2019-03-12",
+        "author": "Edward Park"
+    }
+
+]
